4296cd08acac3da8b3af3e2de2db40171877ebd6
Alepex186 <alejandrobriganty12@gmail.com>
2025-10-06 17:21:04 -0400

    test: Mejora de compatibilidad con CI/CD
    ✅ Agregar configuración de navegadores, para compatibilidad con CI/CD

--- app/src/test/java/myproject/steps/Hooks.java
+++ app/src/test/java/myproject/steps/Hooks.java
@@ -1,4 +1,5 @@
 package myproject.steps;
+
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import myproject.abs.Config;
@@ -14,21 +15,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Hooks {
-    private static ThreadLocal<WebDriver> threadLocalWebdriver=new ThreadLocal<>();
+    private static ThreadLocal<WebDriver> threadLocalWebdriver = new ThreadLocal<>();
     private static ThreadLocal<TestContext> threadLocalContext = new ThreadLocal<>();
 
-
     @Before
-    public void setUpDriver(){
-        String browser=Config.getProperty("BROWSER").toLowerCase();
-        Boolean headless=Boolean.parseBoolean(Config.getProperty("HEADLESS"));
+    public void setUpDriver() {
+        String browser = Config.getProperty("BROWSER").toLowerCase();
+        Boolean headless = Boolean.parseBoolean(Config.getProperty("HEADLESS"));
 
+        WebDriver driver = null;
 
-        WebDriver driver=null;
-
-        if(browser.equals("chrome")){
+        if (browser.equals("chrome")) {
             final Map<String, Object> chromePrefs = new HashMap<>();
-
             chromePrefs.put("credentials_enable_service", false);
             chromePrefs.put("profile.password_manager_enabled", false);
             chromePrefs.put("profile.password_manager_leak_detection", false);
@@ -36,51 +34,59 @@ public class Hooks {
             final ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.setExperimentalOption("prefs", chromePrefs);
             chromeOptions.addArguments("--window-size=1920,1080");
+            chromeOptions.addArguments("--no-sandbox");
+            chromeOptions.addArguments("--disable-dev-shm-usage");
 
-            if(headless) {
+            if (headless) {
                 chromeOptions.addArguments("--headless=new");
             }
-            driver=new ChromeDriver(chromeOptions);
 
+            driver = new ChromeDriver(chromeOptions);
         }
 
-        if(browser.equals("firefox")){
-            final FirefoxOptions firefoxOptions=new FirefoxOptions();
-            if(headless){
+        if (browser.equals("firefox")) {
+            final FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.addArguments("--width=1920");
+            firefoxOptions.addArguments("--height=1080");
+
+            if (headless) {
                 firefoxOptions.addArguments("--headless");
+                firefoxOptions.addArguments("--no-sandbox");
+                firefoxOptions.addArguments("--disable-dev-shm-usage");
             }
-            firefoxOptions.addArguments("--window-size=1920,1080");
-            driver=new FirefoxDriver(firefoxOptions);
 
+            driver = new FirefoxDriver(firefoxOptions);
         }
 
-        if(browser.equals("edge")){
-            final EdgeOptions edgeOptions=new EdgeOptions();
-            if(headless){
-                edgeOptions.addArguments("--headless");
-            }
+        if (browser.equals("edge")) {
+            final EdgeOptions edgeOptions = new EdgeOptions();
             edgeOptions.addArguments("--window-size=1920,1080");
-            driver=new EdgeDriver(edgeOptions);
 
+            if (headless) {
+                edgeOptions.addArguments("--headless=new");
+                edgeOptions.addArguments("--no-sandbox");
+                edgeOptions.addArguments("--disable-dev-shm-usage");
+            }
+
+            driver = new EdgeDriver(edgeOptions);
         }
-        threadLocalWebdriver.set(driver);
 
+        threadLocalWebdriver.set(driver);
         threadLocalContext.set(new TestContext());
     }
 
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
         return threadLocalWebdriver.get();
     }
-
 
     public static ThreadLocal<TestContext> getThreadLocalContext() {
         return threadLocalContext;
     }
 
     @After
-    public void tearDownDriver(){
-        WebDriver driver=threadLocalWebdriver.get();
-        if(driver != null){
+    public void tearDownDriver() {
+        WebDriver driver = threadLocalWebdriver.get();
+        if (driver != null) {
             driver.quit();
             threadLocalWebdriver.remove();
         }
